546a23ef54494661ff9d2797836e89b957c9f779
elexander <elainalexander@live.com>
2015-09-21 12:21:09 -0700

    initial project commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Techforum::Application.config.secret_token = '0151db1318b9065c2d4e56c9a0cd6563e00a462f2be75090e77ed38e1b505ffc6dbf873d0ac63779b29500bcc392c8af40e31335f6044fa77b71772d4b9a6b3b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Techforum::Application.config.session_store :cookie_store, key: '_techforum_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Techforum::Application.config.session_store :active_record_store
