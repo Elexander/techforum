c74746d32d9350cfbc31c47cb2303a23bdf53c50
mindude <eleal@cetys.edu.mx>
2015-10-08 09:52:41 -0700

    Se subio lo que falto para que todo corra bien

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
-  #attr_accessible :content, :post_id, :user_id, :vote_count
+  attr_accessible :content, :post_id, :user_id, :vote_count
   belongs_to :user
   belongs_to :post
 end
