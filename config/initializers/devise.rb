e6c733e4aabc6c5682a93c006cf54dba2dec7604
elexander <elainalexander@live.com>
2015-10-12 13:18:22 -0700

    all changes on divise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -204,6 +204,7 @@ Devise.setup do |config|
   # Require the `devise-encryptable` gem when using anything other than bcrypt
   # config.encryptor = :sha512
 
+  ## UNCOMMENT THIS SECTION WHEN CONFIGURING COSTUM VIEWS
   # ==> Scopes configuration
   # Turn scoped views on. Before rendering "sessions/new", it will first check for
   # "users/sessions/new". It's turned off by default because it's slower if you
