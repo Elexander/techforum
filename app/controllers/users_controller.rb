db192ca0294e7d40d9f9a5e9b6a39cdb312acf76
ISCAlexey <alexey159@gmail.com>
2015-10-20 11:28:00 -0700

    contreller user updated to permit extra values

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+
+	before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) << :name << :last_name
+      end
+    end
 end
