1bc58b8f7a74805f01cb3025cc28dffaefbad40c
ISCAlexey <alexey159@gmail.com>
2015-10-20 11:10:07 -0700

    registration controller updated

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,10 +1,4 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-before_filter :configure_permitted_parameters, :only => [:create]
 
-  protected
-
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :last_name, :email, :password) }
-    end
 
 end
