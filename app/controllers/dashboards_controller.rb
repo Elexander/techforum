1b8937acdca53b704e019c8b259e23ca3dc11a3c
Lazaro Cobix <lazarocobix@gmail.com>
2015-11-09 15:40:33 -0800

    Material in Listpost

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,13 +1,13 @@
 class DashboardsController < ApplicationController
   def main
-  	@today_posts = Post.today
+  	@today_posts = Post.last(10)
   	@discussion_posts = Post.filter_by_type("Discussion")
   	@question_posts = Post.filter_by_type("Question")
   	
   end
 
   def listpost
-  	@today_posts = Post.today
+  	@today_posts = Post.last(10)
   	@discussion_posts = Post.filter_by_type("Discussion")
   	@question_posts = Post.filter_by_type("Question")
 
