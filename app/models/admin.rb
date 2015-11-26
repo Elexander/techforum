336c7e99bdf9b97e00d6d85501d107ad1950df9d
elexander <elainalexander@live.com>
2015-11-26 11:22:38 -0800

    admin tables added/activeadmin gem

---
+++ app/models/admin.rb
@@ -0,0 +1,17 @@
+class Admin < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, 
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me
+  # Include default devise modules. Others available are:
+  # :confirmable, :timeoutable and :omniauthable
+  devise :database_authenticatable, 
+         :recoverable, :rememberable, :trackable, :validatable, :lockable
+
+  # Setup accessible (or protected) attributes for your model
+  # attr_accessible :email, :password, :password_confirmation, :remember_me
+  # attr_accessible :title, :body
+end
